77bf4b56bf980c30da569f411c8b31d4e7e98e14
tatsuhito <takamiya.dev@gmail.com>
2016-09-15 13:18:09 +0900

    シングルトンを作成しました．

---
+++ Assets/Application/Scripts/System/SingletonBehavior.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+
+public abstract class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour {
+	protected static T instance;
+	private bool isInitialized = false;
+
+	public static T I {
+		get {
+			if (instance == null) {
+				instance = (T)FindObjectOfType (typeof(T));
+
+				if (instance == null) {
+					Debug.LogWarning (typeof(T) + "is nothing");
+				}
+			}
+
+			return instance;
+		}
+	}
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	protected virtual void Initialize(){}
+
+	protected virtual void Awake (){
+		if (!CheckInstance () || !isInitialized) {
+			this.Initialize();
+			this.isInitialized = true;
+		}
+	}
+
+	protected bool CheckInstance ()
+	{
+		if (I != null && this == I) {
+			return true;
+		}
+		Destroy(gameObject);
+		return false;
+	}
+
+	virtual protected void OnDestroy() {
+		if (instance != null) {
+			instance = null;
+		}
+	}
+}
---
+++ Assets/Takamiya/ObjectManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectManager : SingletonBehaviour<ObjectManager> {
+
+	public GameObject player;
+
+	protected override void Initialize () {
+		Debug.Log ("init");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
