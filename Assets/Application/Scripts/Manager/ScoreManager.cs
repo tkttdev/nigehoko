d998fe2a5f5a4fa77db5a555669afe1353dfc99c
tatsuhito <takamiya.dev@gmail.com>
2016-09-17 21:33:45 +0900

    ScoreManager作成しました

---
+++ Assets/Application/Scripts/Manager/ScoreManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : SingletonBehaviour<ScoreManager> {
+
+	static int score;
+	const string HIGH_SCORE_KEY = "highScore";
+
+	private void SetHighScore(int score){
+		PlayerPrefs.SetInt(HIGH_SCORE_KEY, score);
+		PlayerPrefs.Save();
+	}
+
+	public int GetHighScore(){
+		return PlayerPrefs.GetInt(HIGH_SCORE_KEY, -1);
+	}
+}
