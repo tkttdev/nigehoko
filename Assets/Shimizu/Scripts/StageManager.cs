b60a20eea90b8fca516b8b33447b16accc15a8ce
prog470 <tshimizu470@stu.kanazawa-u.ac.jp>
2016-09-19 17:37:19 +0900

    StageManagerの変更

--- Assets/Shimizu/Scripts/StageManager.cs
+++ Assets/Shimizu/Scripts/StageManager.cs
@@ -17,7 +17,7 @@ public class StageManager : SingletonBehaviour<StageManager> {
         for (int i = (-1) * (int)height; i <= 2 * height; i += (int)height)
         {
             if (i <= 0)
-            {   //初期位置志望予防
+            {   //初期位置設定
                 Instantiate(blockList[0],
                 new Vector2(0.0f, Camera.main.transform.position.y + i),
                 Quaternion.Euler(0, 0, 0));
@@ -42,9 +42,10 @@ public class StageManager : SingletonBehaviour<StageManager> {
         {
             //カメラの移動
             Camera.main.transform.position = new Vector3(0.0f, Camera.main.transform.position.y + speed*0.1f, -10.0f);
-
-            if (gameObject.transform.position.y - cameyBfore >= height)
+            Debug.Log("cameraY : " + Camera.main.transform.position.y);
+            if (Camera.main.transform.position.y - cameyBfore >= height)
             {
+                Debug.Log("Instantiate");
                 Instantiate(blockList[((int)(Random.Range(0.0f, (float)this.blockList.Length)))],
                     new Vector2(0.0f, Camera.main.transform.position.y + 2.0f * height),
                     Quaternion.identity);
