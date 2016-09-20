caee45073e3db2a45d1394e5f5f6bb33e6dcf330
prog470 <tshimizu470@stu.kanazawa-u.ac.jp>
2016-09-20 17:35:40 +0900

    ステージ生成

--- Assets/Shimizu/Scripts/StageManager.cs
+++ Assets/Shimizu/Scripts/StageManager.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class StageManager : SingletonBehaviour<StageManager> {
 
-    public GameObject[] blockList;
     private float height;
 
     private float cameyBfore;
     private float cameyAfter;
 
-	[SerializeField] public float speed = 1.0f; 
+	[SerializeField] public float speed = 1.0f;
+
+    private int level = 0;
+    private int num = 0;
+
+    private float distcount = 0;
+    [SerializeField] public int interval = 0;
 
     protected override void Initialize() {
         height = 8;  // 8 : blocksize
@@ -18,14 +23,15 @@ public class StageManager : SingletonBehaviour<StageManager> {
         {
             if (i <= 0)
             {   //初期位置設定
-                Instantiate(blockList[0],
+                string num = ((int)(Random.Range(0.0f, 10.0f))).ToString();
+                Instantiate(Resources.Load("Prefabs/StageBlocks/stage" + "0" + "/" + "0" + "0"),
                 new Vector2(0.0f, Camera.main.transform.position.y + i),
                 Quaternion.Euler(0, 0, 0));
-
             }
             else
             {
-                Instantiate(blockList[((int)(Random.Range(0.0f, (float)this.blockList.Length)))],
+                num = (int)(Random.Range(0.0f, 10.0f));
+                Instantiate(Resources.Load("Prefabs/StageBlocks/stage" + level.ToString() + "/" + level.ToString() + num.ToString()),
                 new Vector2(0.0f, Camera.main.transform.position.y + i),
                 Quaternion.Euler(0, 0, 0));
             }
@@ -42,14 +48,25 @@ public class StageManager : SingletonBehaviour<StageManager> {
         {
             //カメラの移動
             Camera.main.transform.position = new Vector3(0.0f, Camera.main.transform.position.y + speed*0.1f, -10.0f);
-            Debug.Log("cameraY : " + Camera.main.transform.position.y);
+            Debug.Log(Camera.main.transform.position.y);
             if (Camera.main.transform.position.y - cameyBfore >= height)
             {
-                Debug.Log("Instantiate");
-                Instantiate(blockList[((int)(Random.Range(0.0f, (float)this.blockList.Length)))],
+                num = (int)(Random.Range(0.0f, 10.0f));
+                Instantiate(Resources.Load("Prefabs/StageBlocks/stage" + level.ToString() + "/" + level.ToString() + num.ToString()),
                     new Vector2(0.0f, Camera.main.transform.position.y + 2.0f * height),
                     Quaternion.identity);
                 cameyBfore = Camera.main.transform.position.y;
+                distcount++;
+            }
+
+            if (distcount == interval)
+            {
+                distcount = 0;
+                level++;
+                if (level > 5)
+                {
+                    level = 5;
+                }
             }
 
             yield return new WaitForSeconds(0.02f);
