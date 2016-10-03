45f6792c6746cc8517f69e718d007f3ff7c6f803
tatsuhito <takamiya.dev@gmail.com>
2016-10-04 04:41:11 +0900

    retrydialog

---
+++ Assets/Application/Scripts/Dialog/RetryDialog.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RetryDialog : DialogBase {
+
+	[SerializeField] private GameObject restartButton;
+	[SerializeField] private GameObject exitButton;
+
+	[SerializeField] private Text resultScoreText;
+	[SerializeField] private Text highScoreText;
+
+	protected override void Start () {
+
+		restartButton = GameObject.FindWithTag ("RestartButton");
+		exitButton = GameObject.FindWithTag ("ExitButton");
+
+		resultScoreText = GameObject.Find ("ResultScoreText").GetComponent<Text> ();
+		highScoreText = GameObject.Find ("HighScoreText").GetComponent<Text> ();
+
+		SetButtonsInactive ();
+
+		base.Start ();
+	}
+
+	public override void Show() {
+		base.Show ();
+		SetButtonsActive ();
+		resultScoreText.text = string.Format ("{0} cm 生きのびた!", ScoreManager.I.GetScore ());
+		highScoreText.text = string.Format ("BEST : {0} cm", ScoreManager.I.GetHighScore ());
+	}
+
+	private void SetButtonsInactive(){
+		restartButton.SetActive (false);
+		exitButton.SetActive (false);
+	}
+
+	private void SetButtonsActive(){
+		restartButton.SetActive (true);
+		exitButton.SetActive (true);
+	}
+
+	public void Restart(){
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void Exit(){
+		SceneManager.LoadScene ("Title");
+	}
+}
