7e4bd2ae6e2331fa77f4078e36ad36a5b0e285ef
tatsuhito <takamiya.dev@gmail.com>
2016-10-13 23:23:16 +0900

    demoが行えるように修正

--- Assets/Application/Scripts/Manager/StageManager.cs
+++ Assets/Application/Scripts/Manager/StageManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class StageManager : SingletonBehaviour<StageManager> {
 
@@ -8,13 +9,16 @@ public class StageManager : SingletonBehaviour<StageManager> {
     private float cameyBfore;
     private float cameyAfter;
 
-	[SerializeField] public float[] speed;
+	public float[] speed;
 
     private int level = 0;
     private int speedlevel = 0;
     private int num = 0;
 
     private bool flag = false;
+
+	public bool isDemo = false;
+	public List<GameObject> demoBlocks = new List<GameObject> ();
 
     private float distcount = 0;
     [SerializeField] public int interval = 0;
@@ -82,42 +86,56 @@ public class StageManager : SingletonBehaviour<StageManager> {
         {
             //カメラの移動
             Camera.main.transform.position = new Vector3(0.0f, Camera.main.transform.position.y + speed[speedlevel]*0.1f, -10.0f);
-            if (Camera.main.transform.position.y - cameyBfore >= height)
-            {
-                num = (int)(Random.Range(0.0f, 10.0f));
-                Instantiate(Resources.Load("Prefabs/StageBlocks/stage" + level.ToString() + "/" + level.ToString() + num.ToString()),
-                    new Vector2(0.0f, Camera.main.transform.position.y + 2.0f * height),
-                    Quaternion.identity);
-                cameyBfore = Camera.main.transform.position.y;
-                distcount++;
-                //Floorのバグ対策
-                if (flag)
-                {
-                    Instantiate(Resources.Load("Prefabs/StageBlocks/WoodFloor"),
-                        new Vector2(0.0f, Camera.main.transform.position.y + 2.0f * height),
-                        Quaternion.identity);
-                    flag = false;
-                }
-                else
-                {
-                    flag = true;
-                }
+			if (!isDemo) {
+				if (Camera.main.transform.position.y - cameyBfore >= height) {
+					num = (int)(Random.Range (0.0f, 10.0f));
+					Instantiate (Resources.Load ("Prefabs/StageBlocks/stage" + level.ToString () + "/" + level.ToString () + num.ToString ()),
+						new Vector2 (0.0f, Camera.main.transform.position.y + 2.0f * height),
+						Quaternion.identity);
+					cameyBfore = Camera.main.transform.position.y;
+					distcount++;
+					//Floorのバグ対策
+					if (flag) {
+						Instantiate (Resources.Load ("Prefabs/StageBlocks/WoodFloor"),
+							new Vector2 (0.0f, Camera.main.transform.position.y + 2.0f * height),
+							Quaternion.identity);
+						flag = false;
+					} else {
+						flag = true;
+					}
 
-            }
+				}
 
-            if (distcount == interval)
-            {  
-                //難易度段階が6段,スピードレベルは5段
-                distcount = 0;
-                if(level < 8)
-                {
-                    level++;
-                }
-                else
-                {
-                    if(speedlevel<5) speedlevel++;
-                }
-            }
+				if (distcount == interval) {  
+					//難易度段階が6段,スピードレベルは5段
+					distcount = 0;
+					if (level < 8) {
+						level++;
+					} else {
+						if (speedlevel < 5)
+							speedlevel++;
+					}
+				}
+			}
+
+			if (isDemo) {
+				if (Camera.main.transform.position.y - cameyBfore >= height) {
+					GameObject obj = demoBlocks[Random.Range (0, demoBlocks.Count)];
+
+					Instantiate (obj, new Vector2 (0.0f, Camera.main.transform.position.y + 2.0f * height), Quaternion.identity);
+					cameyBfore = Camera.main.transform.position.y;
+					//Floorのバグ対策
+					if (flag) {
+						Instantiate (Resources.Load ("Prefabs/StageBlocks/WoodFloor"),
+							new Vector2 (0.0f, Camera.main.transform.position.y + 2.0f * height),
+							Quaternion.identity);
+						flag = false;
+					} else {
+						flag = true;
+					}
+
+				}
+			}
 
             yield return new WaitForSeconds(0.02f);
         }
