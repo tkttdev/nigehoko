ddba0e28297a5ad8452e58dda5f94ca2ca5a1d55
tatsuhito <takamiya.dev@gmail.com>
2016-11-03 07:13:18 +0900

    スクリプト削除

--- Assets/Editor/ReplaceGameObject.cs
+++ Assets/Editor/ReplaceGameObject.cs
@@ -5,13 +5,9 @@ using System.Collections.Generic;
 
 public class ReplaceGameObject : EditorWindow
 {
-	// 置換オブジェクト
 	private Object m_replaceObject = null;
 
-	/// <summary>
-	/// ウィンドウを開く
-	/// </summary>
-	[MenuItem("Custom/Tools/Replace GameObject")]
+	[MenuItem("Tools/GameObject/Replace GameObject")]
 	public static void Open()
 	{
 		ReplaceGameObject window = EditorWindow.GetWindow<ReplaceGameObject>();
@@ -19,9 +15,6 @@ public class ReplaceGameObject : EditorWindow
 		window.Show();
 	}
 
-	/// <summary>
-	/// 初期化
-	/// </summary>
 	public void Init()
 	{
 		m_replaceObject = null;
@@ -29,27 +22,22 @@ public class ReplaceGameObject : EditorWindow
 
 	void OnGUI()
 	{
-		// 置換オブジェクト
 		GameObject replaceObj = EditorGUILayout.ObjectField(m_replaceObject, typeof(GameObject), true) as GameObject;
 		if (replaceObj != null)
 		{
 			if (PrefabUtility.GetPrefabType(replaceObj) == PrefabType.Prefab)
 			{
-				// プレハブがドラッグされた時だけ、置換オブジェクトを更新
 				m_replaceObject = replaceObj;
 			}
 		}
-		// 置換実行ボタン
 		GameObject[] selection = Selection.gameObjects;
-		if ( (m_replaceObject != null) &&                                       // 置換オブジェクトが設定されている
-			(selection != null && selection.Length > 0) )  // ヒエラルキー上のアイテムが1つ以上選択されている
+		if ( (m_replaceObject != null) && (selection != null && selection.Length > 0) )
 		{
 			if (GUILayout.Button("replace"))
 			{
 				List<GameObject> delList = new List<GameObject>();
 				foreach (GameObject selObj in selection)
 				{
-					// プレハブを複製して座標と姿勢をコピーする
 					GameObject newObj = PrefabUtility.InstantiatePrefab(m_replaceObject) as GameObject;
 					if (newObj != null)
 					{
@@ -59,7 +47,6 @@ public class ReplaceGameObject : EditorWindow
 						delList.Add(selObj);
 					}
 				}
-				// 元のオブジェクトをは全て削除
 				foreach (GameObject obj in delList) DestroyImmediate(obj);
 			}
 		}
